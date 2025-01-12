9d23a223d71c29e4c52901187600ec389e6d09b9
Alexandre Tozzini <alexandretozzini@gmail.com>
2025-01-12 18:00:30 -0300

    fix errors and create api method

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  app.use(cors());
+  next();
+});
 
 app.use(router);
 
